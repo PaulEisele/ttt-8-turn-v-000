047d45ea69dfb9c7ea46ea7fb65ad1e2b3c4dd58
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-01 04:13:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -51,13 +51,12 @@ end
 #turn
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.chomp
   input=gets.strip
   index = input_to_index(input)
   move(board, index, "X")
   valid_move?(board, index)
-  if valid_move?(input, board)
-      move(board, input)
+  if valid_move?(index, board)
+      move(board, index)
       display_board(board)
     else
       puts "That is an invalid entry!"
