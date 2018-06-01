981586b7670b3ac9bf31fa010ba0fba9c9d218d3
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-01 03:27:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,5 @@
 #DISPLAY_BOARD
-def display(board)
+def DISPLAY_BOARD(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
