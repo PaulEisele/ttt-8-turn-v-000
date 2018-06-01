2d0fd71dec70c4743a23cfa9dffc7eb3fcd415e0
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-01 03:51:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,9 +30,6 @@ end
 #valid_move?
 def valid_move?(board, index)
   if position_taken?(board, index)
-    puts "Please enter 1-9:"
-    input=gets.strip
-    index = input_to_index(input)
     return false
   elsif index.between?(0,8)
     return true
